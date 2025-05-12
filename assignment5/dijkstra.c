b10392bf5d47dba51a5983bf86b6e3e449f6c90f
Oliver <oliisbali@gmail.com>
2025-05-11 21:30:35 -0700

    Assignment 5 - Dijkstras algorithm

---
+++ assignment5/dijkstra.c
@@ -0,0 +1,36 @@
+/*
+ * In this file, you'll implement Dijkstra's algorithm to find the least
+ * expensive paths in the graph defined in `airports.dat`.  Don't forget to
+ * include your name and @oregonstate.edu email address below.
+ *
+ * Name:
+ * Email:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DATA_FILE "airports.dat"
+
+int main(int argc, char const *argv[]) {
+	/*
+	 * Open file and read the first two int: num of nodes, num of edges
+	 */
+	int n_nodes, n_edges;
+	FILE* file = fopen(DATA_FILE, "r");
+	fscanf(file, " %d %d ", &n_nodes, &n_edges);
+	
+	/*
+	 * Write your code here to find the
+	 * least-cost paths from the start node to all other nodes in the graph.  Make sure
+	 * to print out the least-cost paths you find at the end.
+	 *
+	 * FIXME: 
+ 	 */
+	
+
+	fclose(file);
+
+	 
+  return 0;
+}
